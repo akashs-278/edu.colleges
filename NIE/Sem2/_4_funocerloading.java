93f9e21ce3e21ffcfef2d7b84b5af38d942ade19
akash-278 <sakash68396@gmail.com>
2026-01-04 11:05:24 +0530

    fourth_program

---
+++ NIE/Sem2/_4_funocerloading.java
@@ -0,0 +1,21 @@
+class sum{
+    int add (int a,int b){
+        return a + b;
+    }
+    double add (double a,double b){
+        return a+b;
+    }
+    double add (double a,double b,double c){
+        return a+b+c;
+    }
+    
+}
+public class _4_funocerloading {
+    public static void main(String[] args) {
+
+        sum funO = new sum();
+        System.out.println(funO.add(2,5));
+        System.out.println(funO.add(2.5,5.6,5.6));
+        System.out.println(funO.add(2.5,5.6));
+    }
+}

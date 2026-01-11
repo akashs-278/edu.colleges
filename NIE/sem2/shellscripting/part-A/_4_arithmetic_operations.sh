f3ef0462479c474e5048671db813d5491dd769e9
akash-278 <sakash68396@gmail.com>
2026-01-11 18:09:41 +0530

    fouth_ss

---
+++ NIE/sem2/shellscripting/part-A/_4_arithmetic_operations.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Arithmetic Operations
+
+echo "Enter two numbers"
+read a
+read b
+echo "Subtraction of $b from $a is $(($a-$b))"
+echo "Addition of $a and $b is $(($a+$b))"
+echo "Multiplication of $a and $b is $(($a*$b))"
+echo "Division of $a by $b is $(($a/$b))"

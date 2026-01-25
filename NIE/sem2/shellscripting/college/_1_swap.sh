fe20a512e5e0b7a0d3fb2fd4c56af7ff345c3a4b
akashs-278 <sakash68396@gmail.com>
2026-01-25 10:27:24 +0530

    first program

---
+++ NIE/sem2/shellscripting/college/_1_swap.sh
@@ -0,0 +1,11 @@
+echo "Enter first number : "
+read a
+echo "Enter second number : "
+read b
+echo "Before swap : "
+echo "The values of a is $a and b is $b."
+c=$a
+a=$b
+b=$c
+echo "After swap : "
+echo "The values of a is $a and b is $b."

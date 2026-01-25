73b62ba8f9ccb310b328486af9fa1049e13ebecb
akashs-278 <sakash68396@gmail.com>
2026-01-25 10:58:08 +0530

    3rd program

---
+++ NIE/sem2/shellscripting/college/_3_largest_of_three_numbers.sh
@@ -0,0 +1,11 @@
+echo "Enter 3 numbers : "
+read a b c
+if [ $a -gt $b ] && [ $a -gt $c ]
+then
+echo "$a is the greatest."
+elif [ $b -gt $c ]
+then
+echo "$b is the greatest."
+else
+echo "$c is the greatest."
+fi

1cfcd61082240e89ab91ab882120ae638d633466
akashs-278 <sakash68396@gmail.com>
2026-01-29 00:00:46 +0530

    added next 3 programs

--- NIE/sem2/shellscripting/college/_4_arithmetic_operations.sh
+++ NIE/sem2/shellscripting/college/_4_arithmetic_operations.sh
@@ -4,9 +4,12 @@ echo "Enter the second number "
 read b
 sum=`expr $a + $b`
 echo "sum of $a and $b is $sum."
+
 diff=`expr $a - $b`
 echo "difference of $a and $b is $diff."
+
 prod=`expr $a \* $b`
 echo "product of $a and $b is $prod."
+
 div=`expr $a / $b`
 echo "Dividing $a and $b is $div."

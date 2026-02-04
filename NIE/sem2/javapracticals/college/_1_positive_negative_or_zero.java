f22982dc5f46b864ba5faa968b37e38c34c4541f
akashs@278 <165357004+akashs-278@users.noreply.github.com>
2026-02-05 04:18:42 +0530

    1st and 2nd lab programs

---
+++ NIE/sem2/javapracticals/college/_1_positive_negative_or_zero.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class _1_positive_negative_or_zero {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int num = s.nextInt();
+
+        if (num > 0){
+            System.out.println("It's a positive number.");
+        }else if (num < 0){
+            System.out.println("It's a negative number.");
+        }else{
+            System.out.println("It's a zero.");
+        }
+        s.close();
+    }
+}

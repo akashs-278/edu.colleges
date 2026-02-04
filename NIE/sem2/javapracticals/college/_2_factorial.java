3d3c8eb3dccc20a9edf8bf525df310caae9a8b54
akashs@278 <165357004+akashs-278@users.noreply.github.com>
2026-02-05 04:25:37 +0530

    2nd program

---
+++ NIE/sem2/javapracticals/college/_2_factorial.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class _2_factorial {
+    public static void main(String[] args) {
+       int fact=1,i;
+        for (i=1; i<=10; i++)
+        {
+            fact = fact * i;
+        System.out.println("factorial of "+i+" is "+fact);        
+    }
+    }
+}

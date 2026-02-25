f76189de3a9cd08dc89ca6216593443fac1daa38
akashs@278 <165357004+akashs-278@users.noreply.github.com>
2026-02-25 09:18:22 +0530

    10th program 5 math lib functions

---
+++ NIE/sem1/cpp/part-A/_10_math_lib.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    double s = 25;
+    cout << "Square root value of y=25 is : " << sqrt(s) << endl;
+    int z = -10;
+    cout << "Absolute value of z:-10 is : " << abs(z) << endl;
+    int x = 12, y = 2;
+    cout << "Power value: x^y = (12^2) is: " << pow(x, y) << endl;
+
+    double m = 2.5;
+    cout << "Floor value of x=2.5 is : " << floor(m) << endl;
+    double n = 2.5;
+    cout << "Ceiling value of y=2.5 is : " << ceil(n) << endl;
+    return 0;
+}

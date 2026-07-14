73f759c84a6641896f6357f2ca2a1d5be53aa7bf
Akash S <sakash68396@gmail.com>
2026-07-14 20:57:11 +0530

    updated 1st sql query

--- NIE/sem3/oracle_sql/part-a/activity-1/1_student.sql
+++ NIE/sem3/oracle_sql/part-a/activity-1/1_student.sql
@@ -1,4 +1,12 @@
-create table student(Name varchar2(20), Reg_No number(5) primary key, Class number(2), Major varchar(5));
-insert into student values('Smith', 17, 1, 'CS');
-insert into student values('Brown', 18, 2, 'CS');
-select * from student;
+CREATE TABLE Student (
+    Name VARCHAR2(20),
+    Reg_No NUMBER(5) PRIMARY KEY,
+    Class NUMBER(2),
+    Major VARCHAR2(20)
+);
+
+INSERT INTO Student VALUES('Smith', 17, 1, 'CS');
+INSERT INTO Student VALUES('Brown', 18, 2, 'CS');
+INSERT INTO Student VALUES('John', 8, 1, 'CS');
+
+SELECT * FROM Student;

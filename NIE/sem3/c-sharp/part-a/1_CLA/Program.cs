3b230bd8f17eed76d66909ee5e2f294833d82a12
akashs-278 <sakash68396@gmail.com>
2026-07-15 17:46:40 +0530

    first c-sharp program

---
+++ NIE/sem3/c-sharp/part-a/1_CLA/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace first_program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = int.Parse(args[0]);
+            int num2 = int.Parse(args[1]);
+
+            int sum = num1 + num2;
+
+            Console.WriteLine("Sum of "+num1+" and "+num2+" is "+sum);
+        }
+    }
+}

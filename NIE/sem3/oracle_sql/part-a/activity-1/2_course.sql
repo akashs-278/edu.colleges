46cb3d6bbeadcb8a763a0b58ab07d215be18015f
Akash S <sakash68396@gmail.com>
2026-07-18 20:42:23 +0530

    updated 2_course.sql

--- NIE/sem3/oracle_sql/part-a/activity-1/2_course.sql
+++ NIE/sem3/oracle_sql/part-a/activity-1/2_course.sql
@@ -2,17 +2,12 @@ CREATE TABLE Course (
     Course_Name VARCHAR2(50),
     Course_Number VARCHAR2(10) PRIMARY KEY,
     Credit_Hours NUMBER(2),
-    Department VARCHAR2(50)
+    Department VARCHAR2(20)
 );
 
-insert into course values('Introduction to Computer Science','CS1310',4,'CS');
-insert into course values('Data Structure','CS3320',4,'CS');
-insert into course values('Discrete Mathematics','MATH2410',3,'MATH');
-insert into course values('Database Management System ','CS3380',3,'CS');
+INSERT INTO Course VALUES('Introduction to Computer Science', 'CS1310', 4, 'CS');
+INSERT INTO Course VALUES('Data Structure', 'CS3320', 4, 'CS');
+INSERT INTO Course VALUES('Discrete Mathematics', 'MATH2410', 3, 'MATH');
+INSERT INTO Course VALUES('Database Management System', 'CS3380', 3, 'CS');
 
--- To format the output
-COLUMN COURSE_NAME FORMAT A30;
-COLUMN COURSE_NUMBER FORMAT A15;
-COLUMN DEPARTMENT FORMAT A20;
-
-SELECT * FROM COURSE;
+SELECT * FROM Course;

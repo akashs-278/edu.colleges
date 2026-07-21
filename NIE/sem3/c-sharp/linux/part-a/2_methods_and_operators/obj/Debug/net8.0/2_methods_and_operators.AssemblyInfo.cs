e3500bc2d10f4950fc33a67f8dae25d59dd05e0e
akashs-278 <sakash68396@gmail.com>
2026-07-21 08:41:35 +0530

    added syllabus.pdf and modified some files(c# automatically)

--- NIE/sem3/c-sharp/linux/part-a/2_methods_and_operators/obj/Debug/net8.0/2_methods_and_operators.AssemblyInfo.cs
+++ NIE/sem3/c-sharp/linux/part-a/2_methods_and_operators/obj/Debug/net8.0/2_methods_and_operators.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2_methods_and_operators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15b93666919077808dcacc3911a38075aae80ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba9c52d74a79d8a022b6fd85cee65b0dc5bbe0b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("2_methods_and_operators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2_methods_and_operators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

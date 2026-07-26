4934aec3db8232304364bb9eeb10bdcd9818b3ca
akashs-278 <sakash68396@gmail.com>
2026-07-27 02:25:30 +0530

    added 3rd program

---
+++ NIE/sem3/c-sharp/linux/part-a/3_string_functions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string message = " Hello CSharp World! ";
+        Console.WriteLine("Length: " + message.Length);
+        Console.WriteLine("Uppercase: " + message.ToUpper());
+        Console.WriteLine("Lowercase: " + message.ToLower());
+        Console.WriteLine("Substring(2, 5): " + message.Substring(2, 5));
+        Console.WriteLine("Replace 'CSharp' with 'C#': " + message.Replace("CSharp", "C#"));
+        Console.WriteLine("Contains 'World': " + message.Contains("World"));
+        Console.WriteLine("Index of 'C': " + message.IndexOf('C'));
+        string first = "Hello";
+        string second = "World";
+        string result = string.Concat(first, " ", second);
+        Console.WriteLine("Concatenated String: " + result);
+        Console.ReadKey();
+    }
+}

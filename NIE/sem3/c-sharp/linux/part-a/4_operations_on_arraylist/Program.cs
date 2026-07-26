394c9d24ca3f2678c62240f132b0a6feaf269352
akashs-278 <sakash68396@gmail.com>
2026-07-27 02:37:16 +0530

    updated vscode worspce and addded 4th program

---
+++ NIE/sem3/c-sharp/linux/part-a/4_operations_on_arraylist/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+class ArrayListDemo
+{
+    static void Main()
+    {
+        ArrayList cities = new ArrayList();
+        cities.Add("Mysore");
+        cities.Add("Bangalore");
+        cities.Add("Mandya");
+        Console.WriteLine("Initial ArrayList:");
+        Display(cities);
+        cities.Add("Udupi");
+        Console.WriteLine("\nAfter Inserting/Adding 'Udupi' at index 1:");
+        Display(cities);
+        cities.Remove("Mandya");
+        Console.WriteLine("\nAfter removing 'Mandya':");
+        Display(cities);
+        cities.Sort();
+        Console.WriteLine("\nAfter sorting:");
+        Display(cities);
+        Console.ReadKey();
+    }
+    static void Display(ArrayList list)
+    {
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}

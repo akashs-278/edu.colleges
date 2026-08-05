420a503f0f0be5ec2ef45bddb4d4f98080adb4a7
akashs-278 <sakash68396@gmail.com>
2026-08-05 18:07:01 +0530

    updated actiity-1

--- NIE/sem3/oracle_sql/part-a/activity_1.sql
+++ NIE/sem3/oracle_sql/part-a/activity_1.sql
@@ -89,9 +89,12 @@ update section set section = 'A';
 -- update section set section = 'E' where section_identifier = 119;
 -- update section set section = 'F' where section_identifier = 135;
 
+select * from student;
+select * from course;
+select * from section;
+select * from grade_report;
 
-
-delete Grade_Report where RegNo=(select RegNo from Student where Name='Brown');
+delete Grade_Report where Reg_No=(select Reg_No from Student where Name='Brown');
 
 
 drop table Section cascade constraints;
